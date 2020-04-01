69228204239f3a99821d58cc4451c4c83064c565
Will Storey <willstorey1@gmail.com>
2020-03-31 19:40:50 -0500

    added error handling

--- bmi.php
+++ bmi.php
@@ -14,6 +14,35 @@
     <h1>BMI Calculator</h1>
     <p>Please fill in the following form to calculate your BMI:</p>
     <br>
+    <?php
+        if(isset($_POST['submit3'])){
+            $feet = $_POST['feet'];
+            $inches = $_POST['inches'];
+            $weight = sanitizeString($_POST['weight']);
+            $calculate = true;
+
+            $weightError = '';
+            if ((filter_var($weight, FILTER_VALIDATE_INT) === false) || $weight < 0) {
+                $currentCredErr = "Your weight must be a positive integer";
+                $calculate = false;
+            }
+            
+            if ($calculate === true) {
+                require_once('classes.php');
+
+                $tmp = new BMI($feet, $inches, $weight);
+
+                $bmi = $tmp->getBMI();
+            }
+
+        }
+        function sanitizeString($data) {
+            $data = strip_tags($data);
+            $data = stripslashes($data);
+            $data = htmlentities($data);
+            return $data;
+          }
+    ?>
     <form method='post' action='bmi.php'>
     <label>Current height: 
     <select name='feet'>
@@ -50,21 +79,9 @@
     </label>
     <br>
     <label>Current weight in lbs: <input type='text' name='weight'></label>
+    <span class='error'><?php echo $weightError; ?></span>
     <br><br>
     <input type='submit' name='submit3'>
+    <span>Your BMI: <?php echo $bmi; ?></span>
     </form>
-
-    <?php
-        if(isset($_POST['submit3'])){
-            $feet = $_POST['feet'];
-            $inches = $_POST['inches'];
-            $weight = $_POST['weight'];
-            
-            require_once('classes.php');
-
-            $tmp = new BMI($feet, $inches, $weight);
-
-            echo $tmp->getBMI();
-
-        }
-    ?>
+</body>
